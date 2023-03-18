badced5cd020fabbad9a49624f37579f660add0e
Iguchi-no <iguchipro1029@gmail.com>
2023-03-18 19:37:24 +0900

    email -> name, set_id

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,5 +2,15 @@ class Owner < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
+
+  before_create :set_id
+
+  private
+
+    def set_id
+      while self.owner_id.blank? || Owner.find_by(owner_id: self.owner_id).present? do
+        self.owner_id = SecureRandom.urlsafe_base64(10)
+      end
+    end
 end
